28b0fa46ea9574132675421a8a3cda8c07f92856
Caner <caner.coskun@nodos.dev>
2025-04-03 11:38:00 +0300

    Remove test file

--- test.py
+++
@@ -1,3 +0,0 @@
-from subprocess import CompletedProcess, call, run
-from sys import stderr, stdout
-result = run(["./nodos", "-w", "./workspace", "get", "--version", "1.3.0.b4244", "-y"], stdout=stdout, stderr=stderr, universal_newlines=True)
